6297ff2671fcf35b1676781bc22d19b8530f60a5
Ravi Souza <ravi.souza@super-studio.jp>
2022-03-30 15:54:17 +0900

    small fixes

--- cdk/stacks/vpc/index.ts
+++ cdk/stacks/vpc/index.ts
@@ -1,15 +1,14 @@
 import { Construct } from 'constructs';
-import { Stack, Tags, aws_ec2 as ec2 } from 'aws-cdk-lib';
+import { Stack, aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { Config, constants } from '../../lib/config';
 
 export class Vpc extends Stack {
   constructor(scope: Construct, id: string, props: Config) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'Vpc', {
+    new ec2.Vpc(this, 'Vpc', {
+      vpcName: `${constants.projectName}-${props.envName}-vpc`,
       maxAzs: props.azCount,
     });
-
-    Tags.of(vpc).add('Name', `${constants.projectName}-${props.envName}-vpc`);
   }
 }
